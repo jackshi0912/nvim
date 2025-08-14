7fbfea71f2dd0803c4df0ff8ac63ec76d31f2200
JackShi <jackshi0912@gmail.com>
2025-08-14 11:35:00 +0200

    Added error navigation and floating window.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -39,14 +39,18 @@ vim.keymap.set('n', '<A-h>', '<C-w>h')
 -- Map Alt+l to go to the right window
 vim.keymap.set('n', '<A-l>', '<C-w>l')
 
+-- Map Alt+j to go to the next error.
+vim.keymap.set('n', '<A-j>', function() vim.diagnostic.goto_next(
+    { severity = { min = vim.diagnostic.severity.WARN } })
+end)
+-- Map Alt+k to go to the previous error.
+vim.keymap.set('n', '<A-k>', function() vim.diagnostic.goto_prev(
+    { severity = { min = vim.diagnostic.severity.WARN } })
+end)
+
 vim.keymap.set("n", "<leader>f", function()
     require("conform").format({ bufnr = 0 })
 end)
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<M-k>", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<M-j>", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
