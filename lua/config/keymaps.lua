936abda0a61155a872ba87e7cb4a47a89dbfb08a
Jack <jackshi0912@gmail.com>
2025-10-08 09:24:05 +0200

    remap O

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -55,3 +55,5 @@ end)
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+vim.api.nvim_set_keymap('n', 'O', 'o<Esc>', { noremap = true, silent = true })
+
