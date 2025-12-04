6059ab354c26577cfc80a40e850c8481edb500d1
jack <jackshi0912@gmail.com>
2025-12-04 10:28:32 +0100

    Update pasting in insert.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -56,4 +56,5 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.api.nvim_set_keymap('n', 'O', 'o<Esc>', { noremap = true, silent = true })
+vim.keymap.set( "i", "<C-p>", "<C-R>\"", { noremap = true, silent = true })
 
