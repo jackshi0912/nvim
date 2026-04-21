c6429e732cb1932a764ff2cf67ec1136b4486b49
jack <jackshi0912@gmail.com>
2026-04-21 19:45:14 +0200

    Edit hotkeys.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,7 +8,12 @@ return {
         local function map(lhs, rhs, opts)
                 vim.keymap.set("n", lhs, rhs, opts or {})
         end
-        map("<leader>g", function() harpoon:list():add() end)
+        map("<leader>gg", function() harpoon:list():add() end)
+        map("<leader>gj", function() harpoon:list():replace_at(1) end)
+        map("<leader>gk", function() harpoon:list():replace_at(2) end)
+        map("<leader>gl", function() harpoon:list():replace_at(3) end)
+        map("<leader>g;", function() harpoon:list():replace_at(4) end)
+        map("<leader>g'", function() harpoon:list():replace_at(5) end)
         map("<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
         map("<leader>j", function() harpoon:list():select(1) end)
         map("<leader>k", function() harpoon:list():select(2) end)
