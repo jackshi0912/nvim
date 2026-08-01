c1bac708c9fe10d2197d1b9ed9afc4fed03c7301
jack <jackshi0912@gmail.com>
2026-08-01 22:48:03 +0200

    Edited keymap.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,13 +13,6 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "=ap", "ma=ap'a")
 vim.keymap.set("n", "<leader>rr", "<cmd>LspRestart<cr>")
 
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
-
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
